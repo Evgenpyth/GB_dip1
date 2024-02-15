e8d58f89511c3344587d8316cca88246c540f55f
Evgenpyth <158772611+Evgenpyth@users.noreply.github.com>
2024-02-16 05:06:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from Products import parse_products
+from Reviews import parse_reviews
 
+# URL информации о продуктах и отзывах
+products_url = 'https://catalog-ads.wildberries.ru/api/v6/catalog?menuid=9492'
+reviews_url = 'https://catalog-ads.wildberries.ru/api/v6/catalog?menuid=9492'
